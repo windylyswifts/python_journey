3de2ffd5d5e8a29f36f0366a008bb473a988a311
windylyswifts <isiahalinab@gmail.com>
2025-08-12 19:31:52 +0800

    finished the other half

---
+++ lesson6_Arithmetric_&_Math/L6.py
@@ -0,0 +1,5 @@
+friends = 3
+friends += 1 #This technique is called augmented operation operator
+             #I won't use all operator and give it an example but I'll just put unique ones
+friends **= 2
+print(friends)
