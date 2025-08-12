d68938cbf355a5afb48c5a808a984f3dbed19d16
windylyswifts <isiahalinab@gmail.com>
2025-08-12 19:22:26 +0800

    Finished lesson 5 madlip game

---
+++ lesson5_Madlips_game/L5.py
@@ -0,0 +1,14 @@
+# Madlips games
+# word game where you create a story
+# by filling in blanks with random words
+
+print("Let's play a Madlip game where you'll input random words and you'll see what the sentence")
+adjective1 = input("First Adjective:")
+adjective2 = input("Second Adjective:")
+adjective3 = input("third Adjective:")
+
+print(f"I went to a {adjective1} restuarant early.")
+print(f"It was great but class is about to start so I need to run or else I'll {adjective2}.")
+print(f"I get to class in time but gosh that was {adjective3} experience.")
+
+
