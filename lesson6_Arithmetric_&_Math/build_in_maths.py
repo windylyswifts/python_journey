4b198621849d677dbbfb9b02451411b11413f72a
windylyswifts <isiahalinab@gmail.com>
2025-08-13 18:41:12 +0800

    Didn't finish the last file, will continue

---
+++ lesson6_Arithmetric_&_Math/build_in_maths.py
@@ -0,0 +1,22 @@
+import math #this is like #include likea library
+
+x = 3.14
+y = -4
+z = 5
+
+result = round(x) #round basically round off the nearest whole number
+print(result)
+
+Yresult = abs(y) #abs mean absolute value basically turn anything to positive
+print(Yresult)
+
+Zresult = pow(z, 2) #pow mean "power of"
+print(Zresult)
+
+maxvalue = max(x, y, z)#Maximan value of various values. What's the maximum value between x,y, and z basically
+print(maxvalue)
+
+minvalue = min(x, y, z)#Minimum value of various values
+print(minvalue)
+
+print(math.pi)
